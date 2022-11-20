c88b13bddc6ff798d304a3b07b520b1a80a5d8eb
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2022-11-20 12:50:45 -0300

    feat: creando componente footer e header

---
+++ src/app/componentes/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'amr-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
